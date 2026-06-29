09a8dfbba34323970b40655f8c60163f8ee27226
gnani351 <gnanig809@gmail.com>
2026-06-29 12:37:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Delete confirmation
+function confirmDelete() {
+    return confirm("Are you sure you want to delete this book?");
+}
+
+// Issue confirmation
+function confirmIssue() {
+    return confirm("Do you want to issue this book?");
+}
+
+// Return confirmation
+function confirmReturn() {
+    return confirm("Do you want to return this book?");
+}
